c08b6cdd07755e674bafee31a6169da344c9f855
Manoj Kumar Shivaswami <manojkumarsmks@gmail.com>
2018-04-10 16:23:53 -0600

    Traversal
    Preorder, Inorder and ;evel order

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,12 +1,17 @@
-import java.util.Arrays;
+// Basic tree traversals
+// PreOrder
+// PostOrder
+// InOrder
+// LevelOrder
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.Stack;
 
 public class BinaryTree {
 	static Node root = null;
 	
-	
+	//PreOrder Traversal - Recursion
 	public static void PreOrderRecursive(Node root) {
 		if(root == null)
 			return;
@@ -17,58 +22,23 @@ public class BinaryTree {
 		}			
 	}
 	
-	public static void PreOrderIterative(Node root) {
-		Node current = root;
-		Stack <Node> stack = new Stack <Node>(); 
-		stack.push(current);
+	//PreOrder traversal Iteratively - Using stack
+	public static void PreOrderIteratively(Node root) {
+		Stack<Node> stack = new Stack<Node>();
+		if(root != null)
+			stack.push(root);
+		
 		while(!stack.isEmpty()) {
-			Node temp = stack.pop();
-			System.out.println(temp.data);
-			
-			if(temp.right != null) {
-				stack.push(temp.right);
-			}
-			if(temp.left != null) {
-				stack.push(temp.left);
-			}
+			Node current = stack.pop();
+			System.out.println(current.data);
+			if(current.right != null)
+				stack.push(current.right);
+			if(current.left != null)
+				stack.push(current.left);
+				
 		}
 	}
-	
-	public static void PostOrderRecursive(Node root) {
-		if(root == null) {
-			return;
-		}
-		else {
-			PostOrderRecursive(root.left);
-			PostOrderRecursive(root.right);
-			System.out.println(root.data);
-		}
-	}
-	
-	public static void InOrderIterative(Node root) {
-		Node current = root;
-		Stack <Node> stack = new Stack<Node>();
-		while(true) {
-			if(current != null) {
-				stack.push(current);
-				current = current.left;
-			}
-			else {
-				if(stack.isEmpty())
-					break;
-				else {
-					current = stack.pop();
-					System.out.println(current.data);
-					current = current.right;
-				}
-			}
-		}
-	}
-	
-	public static void LevelOrderTraversal(Node root) {
-		Node current = root;		
-	}
-	
+	//InOrder Traversal - Recursion
 	public static void InOrderRecursively(Node root) {
 		if(root == null) {
 			return;
@@ -80,21 +50,107 @@ public class BinaryTree {
 		}
 	}
 	
-	public static void SizeBinaryTree(Node root, int count) {
+	//InOrder Traversal - Using stack somthing is wrong
+	public void InOrderIteratively(Node root) {
+		Node current = root;
+		Stack <Node> stack = new Stack<Node>();
+		if(current != null)
+			stack.push(current);
+		
+		while(current != null) {
+			stack.push(current);
+			current = current.left;
+		}
+		
+		while(!stack.isEmpty()) {
+			current = stack.pop();
+			System.out.println(current.data); 
+			 
+			if(current.right != null) {
+				current = current.right;
+			}
+			
+			while(current != null) {
+				stack.push(current);
+				current = current.left;
+			}	
+		}
+	}
+	
+	//PostOrder Traversal - Recursion
+	public static void PostOrderRecursive(Node root) {
 		if(root == null) {
 			return;
 		}
 		else {
-			SizeBinaryTree(root.left, count );
-
-			count++;
-			System.out.println(root.data + " "  +count);
-			
-			SizeBinaryTree(root.right, count);
+			PostOrderRecursive(root.left);
+			PostOrderRecursive(root.right);
+			System.out.println(root.data);
 		}
 	}
 	
+	// Level Traversal 
+	public static void LevelOrderTraversal(Node root) {
+		Node current = root;	
+		Queue <Node> queue = new LinkedList<Node>();
+		if(root != null)
+			queue.add(current);
+		
+		while(!queue.isEmpty()) { 
+			current = queue.poll();
+			System.out.println(current.data );
+			if(current.left != null)
+				queue.add(current.left);
+			if(current.right != null)
+				queue.add(current.right);
+		}
+	}
+	
+	// For Testing - Binary Tree 3
+	public static Node BinaryTree3() {
+		BinaryTree binaryTree = new BinaryTree();
+		binaryTree.root = new Node(1);
+		binaryTree.root.right = new Node(2);
+		binaryTree.root.right.right = new Node(5);
+		binaryTree.root.right.right.right = new Node(6);
+		binaryTree.root.right.right.left = new Node(3);
+		binaryTree.root.right.right.left.right = new Node(4);
+		
+		return BinaryTree.root;
+	}
+	
+	// For Testing - Binary Tree 2
+	public static Node BinaryTree2() {
+		/*			15
+		 * 		   /  \
+		 * 		  35    65
+		 * 		 / \     \ 
+		 *		25  45    75
+		 *		/	/ \
+		 *	   15  87 88  
+		 * */
+		BinaryTree binaryTree = new BinaryTree();
+		binaryTree.root = new Node(15);
+		binaryTree.root.left = new Node(35);
+		binaryTree.root.right = new Node(65);
+		binaryTree.root.left.left = new Node(25);
+		binaryTree.root.left.right= new Node(45);
+		binaryTree.root.left.right.left = new Node(87);
+		binaryTree.root.left.right.right =new Node(88);
+		binaryTree.root.right.right = new Node(75);
+		
+		return BinaryTree.root;
+	}
+	
+	// For Testing - Binary Tree 1
 	public static Node BinaryTree1() {
+		
+		/*			10
+		 * 		   /  \
+		 * 		  8    12
+		 * 		 / \   / \ 
+		 *		6   9 11 14
+		 * */
 		BinaryTree binaryTree = new BinaryTree();
 		binaryTree.root = new Node(10);
 		binaryTree.root.left = new Node(8);
@@ -108,52 +164,14 @@ public class BinaryTree {
 		
 	}
 	
-	public static Node BinaryTree3() {
-		BinaryTree binaryTree = new BinaryTree();
-		binaryTree.root = new Node(1);
-		binaryTree.root.right = new Node(2);
-		binaryTree.root.right.right = new Node(5);
-		binaryTree.root.right.right.right = new Node(6);
-		binaryTree.root.right.right.left = new Node(3);
-		binaryTree.root.right.right.left.right = new Node(4);
-		
-		return BinaryTree.root;
-	}
-	
-	public static Node BinaryTree2() {
-		BinaryTree binaryTree = new BinaryTree();
-		binaryTree.root = new Node(19);
-		binaryTree.root.left = new Node(17);
-		binaryTree.root.right = new Node(25);
-		binaryTree.root.left.left = new Node(15);
-		binaryTree.root.left.right= new Node(18);
-		binaryTree.root.left.left.left = new Node(14);
-		binaryTree.root.right.left =new Node(22);
-		binaryTree.root.right.left.left = new Node(20);
-		binaryTree.root.right.left.right = new Node(23);
-		
-		return BinaryTree.root;
-	}
-	
-	
-	public static void main(String[] args) {
-		
-		/*			10
-		 * 		   /  \
-		 * 		  8    12
-		 * 		 / \   / \ 
-		 *		6   9 11 14
-		 * */
+	public static void main(String[] args) { 
 		
 		BinaryTree binaryTree = new BinaryTree();
-		SizeBinaryTree(BinaryTree3(), 0);
-		
-		
-		
-		
+		InOrderRecursively(BinaryTree3());
 	}
 }
 
+// Structure of a node
 class Node {
 	Node left = null;
 	Node right = null;
